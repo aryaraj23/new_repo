57d8a9a1c6c94a2ceba1d954b9c8cba3b130800d
Arya Raj <aryaraj152@gmail.com>
2026-02-15 12:44:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "frontend_public_ip" {
   value = aws_instance.frontend.public_ip
 }
 
-output "backend_public_ip" {
-  value = aws_instance.backend.public_ip
+output "backend_private_ip" {
+  value = aws_instance.backend.private_ip
 }
